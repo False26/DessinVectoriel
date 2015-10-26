e2380c79965bba285628de64a4c760adc8fab87a
atetrel <amaury.tetrel@gmail.com>
2015-10-26 18:55:00 +0100

    first classes

---
+++ src/objets/Objet.java
@@ -0,0 +1,15 @@
+package objets;
+
+/**
+ * Created by Amaury on 26/10/2015.
+ */
+public interface Objet {
+
+    public boolean remplir();
+
+    public boolean dessiner();
+
+    public boolean insérer();
+
+    public boolean etiqueter();
+}
